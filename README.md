e7d14a97466715f843c4b5b40aca3501e5f743a1
Garett Rogers <gootch2@gmail.com>
2024-12-15 08:20:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# hello-auth
+
+A Python package for generating and verifying "hello" authentication messages for Ethereum-based systems.
+
+## Installation
+
+```bash
+pip install hello-auth
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello-auth",
+    version="0.1.0",
+    description="A Python package for generating and verifying 'hello' authentication messages, primarily used for authenticating autonomous agents.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Garett Rogers",
+    author_email="garett@aimx.com",
+    url="https://github.com/aimxlabs/hello-auth-python",
+    packages=find_packages(),
+    install_requires=[
+        "eth-account>=0.5.7",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
