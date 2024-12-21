dbb37bba9adc0ce48f0dbd136c0ba3ef56903883
anythingmachine <gootch2@gmail.com>
2024-12-20 22:41:57 -0800

    Update server.py
    Removing the sdk initialization.  No private key is necessary to validate hello messages.

--- examples/service/server.py
+++ examples/service/server.py
@@ -3,9 +3,7 @@ from hello_message import Hello
 
 app = Flask(__name__)
 
-# Initialize the Hello SDK
 # This private key is for verification purposes only -- should not be used in production
-hello = Hello('0x4c0883a6910395b1e8dcd7db363c124593f3e8e62e4a8c32ef45b3ef82317b03')
 used_nonces = [] # TODO: Nonces should be stored in a database
 
 def require_authentication(f):
@@ -26,7 +24,7 @@ def require_authentication(f):
             signature = auth_header.split(' ')[1]  # Assuming 'Bearer <signed_message>'
             
             # Verify the signed message
-            if not hello.verify_signature(signature, message, address):
+            if not Hello.verify_signature(signature, message, address):
                 return jsonify({'error': 'Invalid or expired token'}), 401
 
             print('Hello', address)
