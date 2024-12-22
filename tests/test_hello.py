8ab843326f75f32eab85f72f47a04bfb50d423f6
Garett Rogers <gootch2@gmail.com>
2024-12-21 16:32:52 -0800

    adding test for expired hello messages

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,14 @@
 import pytest
 from hello_message import Hello
 
+# Test the hello message.  This private key is for verification purposes only -- should not be used in production
+private_key = "0x4c0883a6910395b1e8dcd7db363c124593f3e8e62e4a8c32ef45b3ef82317b03"
+hello = Hello(private_key)
+    
 def test_hello():
-    private_key = "0x4c0883a6910395b1e8dcd7db363c124593f3e8e62e4a8c32ef45b3ef82317b03"
-    hello = Hello(private_key)
     hello_message = hello.generate_hello_message()
-    address = hello.get_address()
-    assert hello.verify_signature(hello_message["signature"], hello_message["message"], address) is True
+    assert hello.verify_signature(hello_message)["valid"] is True
+
+def test_hello_expired():
+    hello_message = hello.generate_hello_message(expires_in_seconds=0)
+    assert hello.verify_signature(hello_message)["valid"] is False
